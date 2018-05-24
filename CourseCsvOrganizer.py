5d306bc60a9995c21c0ee70d8fc6fb7799824fd8
Karanraj Chauhan <chauhank@bu.edu>
2018-05-24 13:12:08 -0400

    initial wokring commit

---
+++ CourseCsvOrganizer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import pickle as pkl
+
+results_raw = pkl.load(open("results.pkl", "rb"))
+results = []
+
+print(results_raw)
+
+for entry in results_raw:
+	results.append({"Name": entry["name"], "City": entry["location"].split(",")[0], "State": entry["location"].split(",")[1]})
+
+pd.DataFrame(results).sort_values(by="State").to_csv("results3.csv")
